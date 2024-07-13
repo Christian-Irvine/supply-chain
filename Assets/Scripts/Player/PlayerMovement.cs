1eef1eebf350602eee8edd3eec757b93cf822c47
Christian Irvine <christian.irvine04@gmail.com>
2024-07-13 14:14:30 +1200

    Added character controller

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float baseSpeed;
+    [SerializeField] private CharacterController characterController;
+
+    private InputAction moveAction;
+
     void Start()
     {
-        
+        moveAction = InputSystem.actions.FindAction("Move");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector2 moveInput = moveAction.ReadValue<Vector2>();
+        moveInput = moveInput.normalized;
+
+        Vector3 moveVector = (transform.forward * moveInput.y * baseSpeed) + (transform.right * moveInput.x * baseSpeed);
+
+        characterController.Move(moveVector * Time.deltaTime);
     }
 }
