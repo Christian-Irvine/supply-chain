bf8a9a79d4cf1a0ade44e0788f9ab4ac4cc475dc
Christian Irvine <christian.irvine04@gmail.com>
2024-07-13 14:27:41 +1200

    Added lerp based camera follow for smooth effect

--- Assets/Scripts/Player/CameraMovement.cs
+++ Assets/Scripts/Player/CameraMovement.cs
@@ -7,11 +7,22 @@ public class CameraMovement : MonoBehaviour
     [SerializeField] private Camera cam;
     [SerializeField][Range(0, 20)] private float horizontalOffset;
     [SerializeField][Range(0, 20)] private float verticalOffset;
+    [SerializeField][Range(0, 20)] private float followSpeed;
 
     void Update()
     {
-        Vector3 newPos = new Vector3 (transform.position.x, transform.position.y + verticalOffset, transform.position.z - horizontalOffset);
+        Vector3 goalPos = new Vector3 (transform.position.x, transform.position.y + verticalOffset, transform.position.z - horizontalOffset);
+
+        float followTime = followSpeed * Time.deltaTime;
+        Vector3 newPos = new Vector3(
+            Mathf.Lerp(cam.transform.position.x, goalPos.x, followTime),
+            Mathf.Lerp(cam.transform.position.y, goalPos.y, followTime),
+            Mathf.Lerp(cam.transform.position.z, goalPos.z, followTime)
+        );
 
         cam.transform.position = newPos;
+
+
+
     }
 }
