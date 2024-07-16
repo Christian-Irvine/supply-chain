7bd46038fe003252ef75b5565f6fb4f9d15c0f00
Christian Irvine <christian.irvine04@gmail.com>
2024-07-16 18:00:49 +1200

    Added GameManager.cs and made money and moneychange event

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    [SerializeField] private int money;
+    public int Money { get => money; set
+        {
+            money = value;
+            MoneyChange?.Invoke();
+        }
+    }
+
+    public UnityEvent MoneyChange;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this) Destroy(this);
+
+        Instance = this;
+    }   
+}
