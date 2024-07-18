a86124b934cea74632b49651a333eeb8f78f5d2f
Christian Irvine <christian.irvine04@gmail.com>
2024-07-18 17:32:30 +1200

    Added some functionality of buildings to GUI

--- Assets/Scripts/UI/BuildMenuManager.cs
+++ Assets/Scripts/UI/BuildMenuManager.cs
@@ -8,30 +8,31 @@ public class BuildMenuManager : MonoBehaviour
     public static BuildMenuManager Instance;
 
     // Visual tree asset is what you use for referencing a UXML file
-    [SerializeField] private VisualTreeAsset buildMenuItem;
+    [SerializeField] private VisualTreeAsset buildMenuSlotTemplate;
     [SerializeField] private string buildMenuItemParentName;
-
-    private UIDocument buildMenu;
-    private VisualElement scrollContainer;
-    TemplateContainer buildMenuTemplate;
 
     private void Awake()
     {
         if (Instance != null && Instance != this) Destroy(this);
-
     }
 
-    private void OnEnable()
+    private IEnumerable OnEnable()
     {
-        buildMenu = GetComponent<UIDocument>();
+        yield return null;
 
-        buildMenuTemplate = buildMenuItem.Instantiate();
-        scrollContainer = buildMenu.rootVisualElement.Q(buildMenuItemParentName);
+        UIDocument buildMenu = GetComponent<UIDocument>();
 
+        foreach (BuildingDataSO building in BuildingManager.Instance.RegisteredBuildings)
+        {
+            BuildMenuSlot newSlot = new BuildMenuSlot(building, buildMenuSlotTemplate);
+
+
+            buildMenu.rootVisualElement.Q(buildMenuItemParentName).Add(newSlot.button);
+        }
     }
 
     private void AddInventoryItem()
     {
-        scrollContainer.Add(buildMenuTemplate);
+        
     }
 }
--- Assets/Scripts/UI/BuildMenuSlot.cs
+++ Assets/Scripts/UI/BuildMenuSlot.cs
@@ -4,12 +4,31 @@ using UnityEngine;
 using UnityEngine.UIElements;
 
 /// <summary>
-/// This class is used to put data on the items inside of the BuildMenu 
+/// This class is used to put data on the buildings inside of the BuildMenu 
 /// </summary>
 public class BuildMenuSlot
 {
+    public BuildingDataSO buildingData;
+    public Button button;
+    public Label nameLabel;
+    public Label costLabel;
+
     public BuildMenuSlot(BuildingDataSO buildingData, VisualTreeAsset template)
     {
+        this.buildingData = buildingData;
 
+        // Creating an instance of the building slot template
+        TemplateContainer buildingButtonContainer = template.Instantiate();
+
+        button = buildingButtonContainer.Q<Button>("Button");
+        button.RegisterCallback<ClickEvent>(OnClick);
+
+        nameLabel = buildingButtonContainer.Q<Label>("Name");
+        costLabel = buildingButtonContainer.Q<Label>("Cost");
+    }
+
+    public void OnClick(ClickEvent evt)
+    {
+        Debug.Log($"Building {buildingData.displayName} has been selected");
     }
 }
