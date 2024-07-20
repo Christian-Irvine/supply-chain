a34db80c49f48e04e1cdd810bb696e9d2f46ef28
Christian Irvine <christian.irvine04@gmail.com>
2024-07-20 17:26:40 +1200

    Added RecipeSO script

---
+++ Assets/Scripts/RecipeSO.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The Scriptable Object class for storing recipe data for machines
+/// </summary>
+
+[System.Serializable]
+public class RecipeItem
+{
+    public ItemDataSO itemData;
+    public int count = 1;
+}
+
+[CreateAssetMenu(fileName = "RecipeSO", menuName = "ScriptableObjects/Recipe", order = 1)]
+public class RecipeSO : ScriptableObject
+{
+    public List<RecipeItem> inputs;
+    public List<RecipeItem> outputs;
+    public float craftSpeed;
+    public BuildingDataSO craftedInBuilding;
+}
